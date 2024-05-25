7c9e59fa8269bfc7279592ecfa4dc6be092c60e0
nikhilpahan <nikhilpahan02@gmail.com>
2024-05-26 01:59:45 +0530

    Starting Class Method

--- 1. Object litrals/object.js
+++ 1. Object litrals/object.js
@@ -1,26 +1,26 @@
 // Object Oriented Programming
 
-var userOne = {
-    email: 'ryu@ninjas.com',
-    name: 'Ryu',
-    login(){
-        console.log(this.email, 'has logged in');
-    },
-    logout(){
-        console.log(this.email, 'has logged out');
-    }
-};
+// var userOne = {
+//     email: 'ryu@ninjas.com',
+//     name: 'Ryu',
+//     login(){
+//         console.log(this.email, 'has logged in');
+//     },
+//     logout(){
+//         console.log(this.email, 'has logged out');
+//     }
+// };
 
 
 //you can use dot notation to update any object.
 // You can also create new object property with this
 
-userOne.name = 'Yoshi';
-userOne.age = 25;
+// userOne.name = 'Yoshi';
+// userOne.age = 25;
 
 //you can also use bracket to update object.
 
-userOne['name'] = 'Ryu';
+// userOne['name'] = 'Ryu';
 
 // but this is not recommeded, ideal situation is when you add all the property inside the object.
 // but in some cases you will have to update like this.
@@ -50,4 +50,7 @@ class User{
 
 var userOne = new User('ryu@ninjas.com', 'Ryu');
 var userTwo = new User('marioninjas@.com', 'Mario');
+
+console.log(userOne);
+console.log(userTwo);
 
