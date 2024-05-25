aa77e7bac703abe44e27d30ff154dffa7c8af572
nikhilpahan <nikhilpahan02@gmail.com>
2024-05-25 23:55:43 +0530

    Knowledge acquired

--- 1. Object litrals/object.js
+++ 1. Object litrals/object.js
@@ -11,3 +11,16 @@ var userOne = {
     }
 };
 
+
+//you can use dot notation to update any object.
+// You can also create new object property with this
+
+userOne.name = 'Yoshi';
+userOne.age = 25;
+
+//you can also use bracket to update object.
+
+userOne['name'] = 'Ryu';
+
+// but this is not recommeded, ideal situation is when you add all the property inside the object.
+// but in some cases you will have to update like this.
