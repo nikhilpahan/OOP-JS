e0edb95505c514569eaf048acfc659d2c9f474a6
nikhilpahan <nikhilpahan02@gmail.com>
2024-05-26 01:18:53 +0530

    Starting Classes

--- 1. Object litrals/object.js
+++ 1. Object litrals/object.js
@@ -24,3 +24,11 @@ userOne['name'] = 'Ryu';
 
 // but this is not recommeded, ideal situation is when you add all the property inside the object.
 // but in some cases you will have to update like this.
+
+
+//Classes
+
+//If we ever have to make mupltiple object with similar kind of properties, we have to create new object again & again.
+// That is where Class comes into play it will give you a map or preset. Now you don't have to re write ever code to create same kind of object.
+
+
