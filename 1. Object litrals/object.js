84061b07e8c590d1724c4109201269ab38344529
nikhilpahan <nikhilpahan02@gmail.com>
2024-05-26 01:54:14 +0530

    Created the fisrt Class

--- 1. Object litrals/object.js
+++ 1. Object litrals/object.js
@@ -29,6 +29,25 @@ userOne['name'] = 'Ryu';
 //Classes
 
 //If we ever have to make mupltiple object with similar kind of properties, we have to create new object again & again.
-// That is where Class comes into play it will give you a map or preset. Now you don't have to re write ever code to create same kind of object.
+// That is where Class comes into play it will give you a map or blueprint or preset which can be editable. Now you don't have to re write ever code to create same kind of object.
+// Classes in javascript are like blueprints
+// It is like a master component and objects will be thier instances.
 
+
+// Constructor Function(): Will let you create object.
+
+// the 'new' keyword
+// - Creates a new empty objects {}
+// - sets the value of 'this' to be the new empty objects
+// - Calls the constructor method
+
+class User{
+    constructor(email, name){
+        this.email = email;
+        this.name = name;
+    }
+}
+
+var userOne = new User('ryu@ninjas.com', 'Ryu');
+var userTwo = new User('marioninjas@.com', 'Mario');
 
